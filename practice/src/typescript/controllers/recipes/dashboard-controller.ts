56b8fd553aeb57afa3cc0e971dbb9df0947c3ad6
diepphan <ngocdiepphan180402@gmail.com>
2024-05-16 11:34:42 +0700

    Implemet close form add recipe #25

--- practice/src/typescript/controllers/recipes/dashboard-controller.ts
+++ practice/src/typescript/controllers/recipes/dashboard-controller.ts
@@ -9,5 +9,6 @@ export default class DashBoardController {
 
   init = async () => {
     this.dashboardView.bindCallback("newToggle");
+    this.dashboardView.bindCallback("closeToggle");
   };
 }
--- practice/src/typescript/views/recipes/dashboard-view.ts
+++ practice/src/typescript/views/recipes/dashboard-view.ts
@@ -4,16 +4,21 @@ import { EventHandler } from "../../helpers/type-dashboard";
 export default class DashBoardView {
   private selectNewEl: HTMLElement;
   private formEl: HTMLElement;
+  private selectCloseEl: HTMLElement;
 
   constructor() {
     this.selectNewEl = document.querySelector(".btn__drawer") as HTMLElement;
     this.formEl = document.querySelector(".modal") as HTMLElement;
+    this.selectCloseEl = document.querySelector(".modal__icon") as HTMLElement;
   }
 
   bindCallback = (event: string, handler: EventHandler): void => {
     switch (event) {
       case "newToggle":
         bindEvent(this.selectNewEl, "click", this.newToggle);
+        break;
+      case "closeToggle":
+        bindEvent(this.selectCloseEl, "click", this.closeToggle); // Toggle icon close
         break;
       default:
         break;
@@ -25,6 +30,19 @@ export default class DashBoardView {
    * @param {Event} event - Click event object.
    */
   newToggle = (event: Event): void => {
+    event.preventDefault();
+    if (this.formEl.classList.contains("show-form")) {
+      this.formEl.classList.remove("show-form");
+    } else {
+      this.formEl.classList.add("show-form");
+    }
+  };
+
+  /**
+   * The closeToggle function changes the display state of a user interface element when a click event occurs.
+   * @param {Event} event - Click event object.
+   */
+  closeToggle = (event: Event): void => {
     event.preventDefault();
     if (this.formEl.classList.contains("show-form")) {
       this.formEl.classList.remove("show-form");
